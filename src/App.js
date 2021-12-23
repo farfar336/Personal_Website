e6accaa745cb07aebc0387f80c7d9a8a6905b7c3
Farrukh <farrukh.naghiyev3@gmail.com>
2021-12-22 18:18:50 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavbarComp from './Cosmponents/NavbarComp';
+import NavbarComp from './Components/NavbarComp';
 
 function App() {
   return (
