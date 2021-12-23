107a5874431afc506f00b441c6cbe543a437d371
Farrukh <farrukh.naghiyev3@gmail.com>
2021-12-22 18:55:38 -0700

    test

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div class="text-left">
+                <h1>Farrukh</h1>
+                <h1 class="text-primary">Programmer</h1>
+            </div>
+        )
+    }
+}
---
+++ src/components/NavbarComp.js
@@ -0,0 +1,52 @@
+// React libraries
+import React, { Component } from 'react'
+import { Navbar, Nav } from 'react-bootstrap'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+// JS files
+import Home from './Home';
+import Contact from './Contact';
+import About from './About'
+import Projects from './Projects'
+
+export default class NavbarComp extends Component {
+    render() {
+        return (
+            <Router>
+
+                {/* Content displayed */}
+                <div>
+                    <Navbar className="bg-light justify-content-center" >
+                        <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About</Nav.Link>
+                        <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                        <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+                    </Navbar>
+                </div>
+
+                {/* Routing */}
+                <div>
+                    <Switch>
+                        <Route path="/about">
+                            <About />
+                        </Route>
+                        <Route path="/contact">
+                            <Contact />
+                        </Route>
+                        <Route path="/home">
+                            <Home />
+                        </Route>
+                        <Route path="/projects">
+                            <Projects />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        )
+    }
+}
