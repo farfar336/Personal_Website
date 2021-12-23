65a7a867842ddb9398801f8c427c40ee603c60e4
Farrukh <farrukh.naghiyev3@gmail.com>
2021-12-22 18:27:23 -0700

    test

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,6 @@ export default class Home extends Component {
     render() {
         return (
             <div class="text-left">
-                <p class="text-right">test</p>
                 <h1>Farrukh</h1>
                 <h1 class="text-primary">Programmer</h1>
             </div>
