d2db00e78c839371d624ca62ba062f0f9d210a0e
Farrukh <farrukh.naghiyev3@gmail.com>
2022-01-02 15:48:52 -0700

    Fixed footer on projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -63,7 +63,7 @@ function Projects () {
             </div>
 
             {/* Footer */}
-            <div className = 'footer-short-page'>
+            <div className = 'footer-tall-page'>
                 <a href = "https://github.com/farfar336/"> <img src = "https://cdn-icons-png.flaticon.com/512/25/25231.png" alt = 'GitHub Logo' height = {30} /></a>
                 <a href = "https://www.linkedin.com/in/farrukhnaghiyev3/"><img src = "https://cdn-icons-png.flaticon.com/512/174/174857.png" 
                 alt = 'LinkedIn Logo' height = {30} /></a>
